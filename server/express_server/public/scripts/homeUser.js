5c67d349b7072a639e4e03a820d47763eacbef5a
RogwiCajas <rogwinalex2@hotmail.com>
2021-06-06 13:23:32 -0500

    appointment:root bug solved

--- server/express_server/public/scripts/homeUser.js
+++ server/express_server/public/scripts/homeUser.js
@@ -1,6 +1,6 @@
 
 $(document).ready(function() {
-    let url = "/appointment/PENDING";
+    let url = "/appointment/state/PENDING";
     getFetch(url, {}).then((res) => {
         if (res) {
             loadAppointment(res);
