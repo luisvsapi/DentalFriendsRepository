89823c20625b8e62aa70bd811f02a35bb5256b8f
Pabelco Zambrano <pagazamb@espol.edu.ec>
2021-08-10 12:48:34 -0500

    Load Testing uploading
    Load Testing uploaded + config for the iOS build

--- app/dental_friends_app/lib/constants/config.dart
+++ app/dental_friends_app/lib/constants/config.dart
@@ -2,4 +2,4 @@
 //String apiUrl = 'http://dentalfriends.ec:80';
 //String apiUrl = 'http://67.205.165.126';
 //DESARROLLO
-String apiUrl = 'http://192.168.100.61:3000';
+String apiUrl = 'http://dentalfriends.ec:80';
