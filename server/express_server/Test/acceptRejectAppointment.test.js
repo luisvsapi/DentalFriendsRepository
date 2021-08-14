767711d41564e86c369dd7b44b6f082bb75799d0
RogwiCajas <rogwinalex2@hotmail.com>
2021-08-14 15:31:01 -0500

    test files created

--- server/express_server/Test/acceptRejectAppointment.test.js
+++ server/express_server/Test/acceptRejectAppointment.test.js
@@ -6,7 +6,7 @@ const api = supertest(app)
 let date = new Date('2021-11-03');
 date.setDate(date.getDate()+1);
 const appointment = {
-    idCardPacient: "2200723333",
+    idCardPacient: "2200723344",
     namePacient: "Test Name",
     lastnamePacient: "Test Lastname",
     agePacient: 22,
@@ -19,23 +19,7 @@ const appointment = {
     treat: "Restauraciones dentales",
     doctor: 22
 }
-/**
- * Test Cuando se envia valores incorrectos
- */
-/*
- test('La solicitud de cita se rechaza por fecha igual o anterior', async () =>{
-    const incorrect = appointment;
-    incorrect.date = "2020/12/03"
-    await api
-        .post('/appointment/setAppointment/')
-        .send(appointment)
-        .set('Accept', 'application/json')
-        //.expect('Content-Type', /application\json/)
-        //.expect(200)
-        .expect({ message: 1, infoAppointment: "Ok" })
-        
-})
-*/
+
 /**
  * Test Cuando se envia valores correctos y el paciente no tiene citas agendadas
  */
@@ -53,7 +37,7 @@ test('La solicitud de cita se agenda correctamente', async () =>{
 /**
  * Test Cuando se envia valores correctos y el paciente  tiene citas agendadas
  */
-test('La solicitud de cita se rechaza correctamente ', async () =>{
+test('La solicitud de cita se rechaza correctamente por cédula repetida', async () =>{
     await api
         .post('/appointment/setAppointment/')
         .send(appointment)
