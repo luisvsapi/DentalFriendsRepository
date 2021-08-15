fe43c6a01ee3ecc9c22ac3472f096cb1ca026c45
Pabelco Zambrano <pagazamb@espol.edu.ec>
2021-08-15 13:04:40 -0500

    Deployment of the iOS App
    Certain configs were made in order to deploy without errors the app in the client's iPhone

--- app/dental_friends_app/lib/constants/config.dart
+++ app/dental_friends_app/lib/constants/config.dart
@@ -1,8 +1,8 @@
 //PRODUCCION
-//String apiUrl = 'http://dentalfriends.ec:80';
-//String apiUrl = 'http://67.205.165.126';
+// String apiUrl = 'http://dentalfriends.ec:80';
+String apiUrl = 'http://67.205.165.126';
 //DESARROLLO
-String apiUrl = 'http://192.168.100.61:3000';
+// String apiUrl = 'http://192.168.100.61:3000';
 var dateActual = DateTime.now();
 var startDate = DateTime.utc(dateActual.year, dateActual.month, dateActual.day, 6, 0, 0);
 var endDate = DateTime.utc(dateActual.year, dateActual.month, dateActual.day, 20, 0, 0);
